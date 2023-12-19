95a6f0125875bdfcfc2878b6ba1b4f6844ff9777
shrenikpatel17 <shrenik.d.patel@gmail.com>
2023-12-19 17:33:45 -0500

    Unfinished code, attempting to display list of ToDo items as per ToDo model created in previous commit

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -13,6 +13,7 @@ struct Todo: Identifiable, Hashable {
     let title: String
     let date: Date
     let status: String
+    let category: String
 }
 
 // Helper function to create Date instances from components
@@ -32,20 +33,20 @@ func createDate(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date
 struct TodoDetailView: View {
     var todo: Todo
 
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text(todo.title)
-                .font(.title)
-            Text(formatDate(todo.date))
-                .font(.subheadline)
-                .foregroundColor(.gray)
-            Text(todo.status)
-                .font(.subheadline)
-                .foregroundColor(.gray)
-        }
-        .padding()
-        .navigationTitle("Todo Details")
-    }
+//    var body: some View {
+//        VStack(alignment: .leading, spacing: 10) {
+//            Text(todo.title)
+//                .font(.title)
+//            Text(formatDate(todo.date))
+//                .font(.subheadline)
+//                .foregroundColor(.gray)
+//            Text(todo.status)
+//                .font(.subheadline)
+//                .foregroundColor(.gray)
+//        }
+//        .padding()
+//        .navigationTitle("Todo Details")
+//    }
 
     private func formatDate(_ date: Date) -> String {
         let formatter = DateFormatter()
@@ -53,6 +54,37 @@ struct TodoDetailView: View {
         formatter.timeStyle = .short
         return formatter.string(from: date)
     }
+    
+    let todos: [Todo] = [
+        Todo(id: 1, title: "Buy groceries", date: createDate(year: 2023, month: 7, day: 20, hour: 9, minute: 0), status: "incomplete", category: "Home"),
+        Todo(id: 2, title: "Finish homework", date: createDate(year: 2023, month: 7, day: 21, hour: 15, minute: 30), status: "incomplete", category: "School"),
+        Todo(id: 3, title: "Call mom", date: createDate(year: 2023, month: 7, day: 22, hour: 12, minute: 0), status: "incomplete", category: "Personal"),
+        ]
+    
+    var body: some View {
+            NavigationView {
+                List {
+                    ForEach(todos, id: \.self) { todo in
+                        NavigationLink(destination: TodoDetailView(todo: todo)) {
+                            HStack(alignment: .center) {
+                                VStack(alignment: .leading) {
+                                    Text(todo.title)
+                                        .font(.title3)
+                                    Text(formatDate(todo.date))
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                            
+                            }
+                        }
+                    }
+                }
+                .listStyle(.inset)
+                .padding()
+                .navigationTitle("Todo List")
+            }
+        }
 }
 
 struct ContentView: View {
@@ -63,32 +95,33 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
 
-    var body: some View {
-        NavigationView {
-            List {
-                Text("Categories")
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
+//    var body: some View {
+//        NavigationView {
+//            List {
+//                Text("Categories")
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+//                    } label: {
+//                        Text(item.timestamp!, formatter: itemFormatter)
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//            Text("Select an item")
+//        }
+//    }
+    
 
     private func addItem() {
         withAnimation {
